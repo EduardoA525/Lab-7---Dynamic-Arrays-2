2b6ca7051aebfa6c746d7abad2fc271dd26a459d
EduardoA525 <eavila913@insite.4cd.edu>
2026-09-13 20:08:27 -0700

    Initial set up

---
+++ Lab7.cpp
@@ -0,0 +1,37 @@
+/*
+Write a program that will reverse a dynamic string array.
+
+Write a function reverseArray() which receives a dynamic string array, 
+reverses the elements, and returns a pointer to the modified array. 
+The function does not output anything.
+
+Write a function displayArray() which receives a dynamic string array, and outputs its elements.
+
+In main(), create a dynamic string array and populate it with 5 names of your choice. 
+Call the two functions appropriately to reverse and output the array.
+
+Use pointer notation rather than array notation for this assignment.
+*/
+
+//Eduardo Avila
+//COMSC - 210 - 5293
+//Lab 7 - Dynamic Arrays and Functions 2
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+//Prototypes
+
+//reverseArray()
+//displayArray()
+
+
+int main(){
+
+
+    return 0;
+}
+
+//Defintions
